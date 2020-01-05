e79bc27aa0c7616091faef5c4e23cc018e4c8c10
Cyril Vermande <cyril@cyrilwebdesign.com>
2020-01-05 12:46:51 +0100

    Remove puppeteer

--- README.md
+++ README.md
@@ -1,13 +1,89 @@
 # cyve/ecoindex
 
-Calculates a webpage ecoindex (from http://www.ecoindex.fr)
+This Node.js module calculates a webpage's ecoindex (from http://www.ecoindex.fr)
 
-## Usage
+## Installation
 ```bash
-$ node ecoindex <url>
+$ npm install https://github.com/cyve/ecoindex/tarball/master
 ```
 
-## Help
-```bash
-$ node ecoindex --help
+## Usage
+```javascript
+const ecoindex = require('ecoindex');
+
+/**
+ * dom = number of HTML elements in DOM
+ * req = number of external requests (images, css, frames, etc.)
+ * size = total size of the page (including external requests) in ko
+ * returns a float between 0 and 100 (higher is better)
+ **/
+var index = ecoindex.calculate(dom, req, size);
+```
+
+Full example with [puppeteer](https://github.com/puppeteer/puppeteer) :
+```javascript
+const zlib = require('zlib');
+const puppeteer = require('puppeteer');
+const ecoindex = require('ecoindex');
+
+(async () => {
+    var url = 'https://example.com';
+    var browser = await puppeteer.launch();
+    var page = await browser.newPage();
+
+    // get number of external requests
+    var req = 0;
+    await page.on('request', request => {
+        if (!request.url().startsWith('data:')) {
+            req++;
+        }
+    });
+
+    // get total uncompressed size
+    var size = 0;
+    await page.on('response', response => {
+        if (response.ok()) {
+            switch (response.headers()['content-encoding']) {
+                case 'br':
+                    response.buffer().then(buffer => {
+                        zlib.brotliCompress(buffer, function (_, result) {
+                            size += result.length;
+                        });
+                    });
+                    break;
+                case 'gzip':
+                    response.buffer().then(buffer => {
+                        zlib.gzip(buffer, function (_, result) {
+                            size += result.length;
+                        });
+                    });
+                    break;
+                case 'deflate':
+                    response.buffer().then(buffer => {
+                        zlib.deflate(buffer, function (_, result) {
+                            size += result.length;
+                        });
+                    });
+                    break;
+                default:
+                    response.buffer().then(buffer => {
+                        size += buffer.length;
+                    });
+                    break;
+            }
+        }
+    });
+
+    await page.goto(url);
+
+    // get number of DOM elements
+    var dom = await page.evaluate(() => document.querySelectorAll('*').length);
+
+    await browser.close();
+
+    // calculate ecoindex
+    var index = ecoindex.calculate(dom, req, Math.round(size / 1024));
+
+    process.stdout.write(index.toString());
+})();
 ```
--- ecoindex.js
+++ ecoindex.js
@@ -1,81 +1,56 @@
-#!/usr/bin/env node
+var domQuantiles = [0, 47, 75, 159, 233, 298, 358, 417, 476, 537, 603, 674, 753, 843, 949, 1076, 1237, 1459, 1801, 2479, 594601];
+var reqQuantiles = [0, 2, 15, 25, 34, 42, 49, 56, 63, 70, 78, 86, 95, 105, 117, 130, 147, 170, 205, 281, 3920];
+var sizeQuantiles = [0, 1.37, 144.7, 319.53, 479.46, 631.97, 783.38, 937.91, 1098.62, 1265.47, 1448.32, 1648.27, 1876.08, 2142.06, 2465.37, 2866.31, 3401.59, 4155.73, 5400.08, 8037.54, 223212.26];
 
-const zlib = require('zlib');
-const input = require('commander');
-const puppeteer = require('puppeteer');
-const calculator = require('./calculator.js');
-
-(async () => {
-    input
-        .arguments('<arg>')
-        .action(arg => url = arg)
-        .option('-f, --format <format>', 'Output format (ex: json).')
-        .option('-c, --collector <collector>', 'Webhook URL (the result will be send as JSON payload).')
-        .parse(process.argv);
-
-    try {
-        new URL(url);
-    } catch (e) {
-        console.error(e.message);
-        process.exit(1);
+/**
+ * @param {array} quantiles
+ * @param {number} value
+ * @return {number}
+ */
+var calculateIndex = function (quantiles, value) {
+    for (var i = 1; i < quantiles.length; i++) {
+        if (value < quantiles[i]) {
+            return (i + (value - quantiles[i - 1]) / (quantiles[i] - quantiles[i - 1]));
+        }
     }
 
-    var browser = await puppeteer.launch();
-    var page = await browser.newPage();
+    return quantiles.length;
+};
 
-    var req = 0;
-    await page.on('request', request => {
-        if (!request.url().startsWith('data:')) {
-            req++;
-        }
-    });
+/**
+ * @param {number} dom Number of DOM elements
+ * @param {number} req Number of HTTP requests
+ * @param {number} size Page size in ko
+ * @return {number}
+ */
+module.exports.calculate = function (dom, req, size) {
+    var domIndex = calculateIndex(domQuantiles, dom),
+        reqIndex = calculateIndex(reqQuantiles, req),
+        sizeIndex = calculateIndex(sizeQuantiles, size),
+        total = 100 - 5 * (3 * domIndex + 2 * reqIndex + sizeIndex) / 6;
 
-    var size = 0;
-    await page.on('response', response => {
-        if (response.ok()) {
-            switch (response.headers()['content-encoding']) {
-                case 'br':
-                    response.buffer().then(buffer => {
-                        zlib.brotliCompress(buffer, function (_, result) {
-                            size += result.length;
-                        });
-                    });
-                    break;
-                case 'gzip':
-                    response.buffer().then(buffer => {
-                        zlib.gzip(buffer, function (_, result) {
-                            size += result.length;
-                        });
-                    });
-                    break;
-                case 'deflate':
-                    response.buffer().then(buffer => {
-                        zlib.deflate(buffer, function (_, result) {
-                            size += result.length;
-                        });
-                    });
-                    break;
-                default:
-                    response.buffer().then(buffer => {
-                        size += buffer.length;
-                    });
-                    break;
-            }
-        }
-    });
+    return parseFloat(total.toPrecision(3));
+};
 
-    await page.goto(url);
-
-    var dom = await page.evaluate(() => document.querySelectorAll('*').length);
-
-    await browser.close();
-
-    var index = calculator.calculate(dom, req, Math.round(size / 1024));
-
-    if (input.format === 'json') {
-        process.stdout.write(JSON.stringify({url: url, dom: dom, req: req, size: size, ecoindex: index}));
-        return;
+/**
+ * @param {number} index EcoIndex
+ * @return {string}
+ */
+module.exports.getNote = function (index) {
+    switch (true) {
+        case index >= 75:
+            return 'A';
+        case index >= 65:
+            return 'B';
+        case index >= 50:
+            return 'C';
+        case index >= 35:
+            return 'D';
+        case index >= 20:
+            return 'E';
+        case index >= 5:
+            return 'F';
+        default:
+            return 'G';
     }
-
-    process.stdout.write(index.toString());
-})();
+};
